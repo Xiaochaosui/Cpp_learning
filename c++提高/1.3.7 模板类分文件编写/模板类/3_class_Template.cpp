ed3d27862016aef056f3a853bc2420db631ab411
xiaochaosui <chaosuixiao@gmail.com>
2021-09-14 10:13:52 +0800

    类模板分文件编写

--- c++提高/1.3.7 模板类分文件编写/模板类/3_class_Template.cpp
+++ c++提高/1.3.7 模板类分文件编写/模板类/3_class_Template.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-
-#include "Xcs.cpp" // 第一种方式，导入你编写的类，把整个类都写到.cpp文件中不需要.h 文件
- 
+// 第一种方式，导入你编写的类，把整个类都写到.cpp文件中不需要.h 文件
+//#include "Xcs.cpp" 
+ // 第二种解决方式 将.h和.cpp的内容写到一起，将后缀名改为.hpp文件
+#include "Xcs.hpp"
 
 using namespace std;
 
@@ -10,10 +11,6 @@ void test1()
 {   //  分文件编写  模板类 Xcs
     Xcs<string,int> x("xcs",22); // 而直接这么调用是会报错的也就是上面提到的问题
     x.show();
-
-    // 分文件编写 普通类Person
-    // Person p("xcs",1);
-    // p.show();
 }
 //
 int main()
